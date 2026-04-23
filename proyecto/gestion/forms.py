025fc88e3017aa05db56e969bd3e914b661d11d2
juangallardo19 <juangallardocsfn@gmail.com>
2026-04-23 12:52:24 -0500

    fix errors

--- proyecto/gestion/forms.py
+++ proyecto/gestion/forms.py
@@ -5,8 +5,14 @@ class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
         fields = ['nombre', 'correo', 'nacionalidad', 'fecha_nacimiento', 'biografia']
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
+        }
 
 class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
         fields = ['titulo', 'fecha_publicacion', 'genero', 'isbn', 'autor']
+        widgets = {
+            'fecha_publicacion': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
+        }
